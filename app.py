3117554ad2a7682bfcd551fc8db611d36b160561
Dev <dev.solanki.works@gmail.com>
2026-03-26 22:40:10 +0530

    Main APP and HTML

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-import img2pdf
-from PIL import Image
 import os
+from flask import Flask, render_template, request, send_file
+import img2pdf
 
-def image_to_pdf(output_filename, image_list):
-    with open(output_filename, "wb") as f:
-        pdf_bytes = img2pdf.convert(image_list)
-        f.write(pdf_bytes)
-    print(f"Successfully created: {output_filename}")
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-if __name__ == "__main__":
-    test_images = ["test.jpg"]
-    image_to_pdf("my_result.pdf", test_images)
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    if 'file' not in request.files:
+        return "No file part"
+    
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected File"
+    
+    img_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(img_path)
+
+    pdf_path = img_path.rsplit('.', 1)[0] + ".pdf"
+
+    with open(pdf_path, "wb") as f:
+        f.write(img2pdf.convert(img_path))
+    
+    return send_file(pdf_path, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
